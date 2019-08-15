82db3ebc56397cb694c60ae6b67889904e74849d
Ilya Belkin <ilya.belkin-trade@yandex.ru>
2019-08-15 15:32:10 +0300

    CDSCV-23 script for debugging

---
+++ coco_gt_to_dt.py
@@ -0,0 +1,47 @@
+import argparse
+import json
+import pycocotools.mask as mask_utils
+
+from pycocotools.coco import COCO
+
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--coco-gt", type=str, required=True)
+    parser.add_argument("--coco-dt", type=str, required=True)
+    parser.add_argument("--iou-type", type=str, choices=["segm", "bbox"], default="segm")
+    parser.add_argument("--fmt", action="store_true")
+    return parser
+
+
+def main(args):
+    coco_gt = COCO(args.coco_gt)
+
+    coco_dt_lst = []
+    for ann in coco_gt.anns.values():
+        annotation = {
+            "image_id": ann["image_id"],
+            "category_id": ann["category_id"],
+            "score": 1.0
+        }
+        if args.iou_type == "segm":
+            rle = mask_utils.encode(coco_gt.annToMask(ann))
+            rle['counts'] = rle['counts'].decode('ascii')
+            annotation["segmentation"] = rle
+        elif args.iou_type == "bbox":
+            annotation["bbox"] = ann["bbox"]
+        coco_dt_lst.append(annotation)
+
+    indent = None
+    separators = (",", ":")
+    if args.fmt:
+        indent = 2
+        separators = (",", ": ")
+    with open(args.coco_dt, "w") as f:
+        json.dump(coco_dt_lst, f, indent=indent, separators=separators)
+
+
+if __name__ == "__main__":
+    parser = build_parser()
+    args = parser.parse_args()
+    main(args)
