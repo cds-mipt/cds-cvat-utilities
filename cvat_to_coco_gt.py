df8a9cb2da508c56ea3b458c798dfde43a4f1553
Ilya Belkin <ilya.belkin-trade@yandex.ru>
2019-10-23 22:24:33 +0300

    fix converter

--- cvat_to_coco_gt.py
+++ cvat_to_coco_gt.py
@@ -1,14 +1,18 @@
 import argparse
 import json
 import numpy as np
+import os
 
 from cvat import CvatDataset
+from utils import coco_cat_to_label
 
 
 def build_parser():
     parser = argparse.ArgumentParser("Convert Cvat annotations to COCO ground truth")
     parser.add_argument("--cvat", type=str, required=True,
                         help="Cvat annotations")
+    parser.add_argument("--discard", type=str, default="",
+                        help="File prefix to be discarded")
     parser.add_argument("--coco-in", type=str, required=False, default=None,
                         help="Ground truth in COCO .json format")
     parser.add_argument("--coco-out", type=str, required=True,
@@ -19,19 +23,24 @@ def build_parser():
 
 
 def main(args):
-    if args.coco_in is not None:
-        raise NotImplementedError
-
     cvat = CvatDataset()
     cvat.load(args.cvat)
 
-    label_to_cat_id = {label: i + 1 for i, label in enumerate(cvat.get_labels())}
-    categories = [{"id": cat_id, "name": label, "supercategory": "entity"}
-                  for label, cat_id in label_to_cat_id.items()]
+    if args.coco_in is not None:
+        with open(args.coco_in, "r") as f:
+            coco_gt = json.load(f)
+        categories = coco_gt["categories"]
+        name_to_coco_id = {image["file_name"]: image["id"] for image in coco_gt["images"]}
+    else:
+        categories = [{"id": cat_id, "name": label, "supercategory": "entity"}
+                      for cat_id, label in enumerate(cvat.get_labels(), start=1)]
+        name_to_coco_id = {os.path.relpath(cvat.get_name(image_id), args.discard): image_id
+                           for image_id in cvat.get_image_ids()}
+    label_to_cat_id = {coco_cat_to_label(cat): cat["id"] for cat in coco_gt["categories"]}
 
     images, annotations = [], []
     for image_id in cvat.get_image_ids():
-        name = cvat.get_name(image_id)
+        name = os.path.relpath(cvat.get_name(image_id), args.discard)
         size = cvat.get_size(image_id)
 
         image = {"id": image_id, "file_name": name}
@@ -42,7 +51,7 @@ def main(args):
             x, y, width, height = box["xtl"], box["ytl"], box["xbr"] - box["xtl"], box["ybr"] - box["ytl"]
             annotation = {
                 "id": len(annotations) + 1,
-                "image_id": image_id,
+                "image_id": name_to_coco_id[name],
                 "category_id": label_to_cat_id[box["label"]],
                 "segmentation": [[x, y, x + width, y, x + width, y + height, x, y + height]],
                 "area": width * height,
@@ -57,14 +66,17 @@ def main(args):
             width, height = points.max(axis=0) - [x, y]
             annotation = {
                 "id": len(annotations) + 1,
-                "image_id": image_id,
+                "image_id": name_to_coco_id[name],
                 "category_id": label_to_cat_id[polygon["label"]],
-                "segmentation": points.flatten().tolist(),
+                "segmentation": [points.flatten().tolist()],
                 "area": width * height,
                 "bbox": [x, y, width, height],
                 "iscrowd": 0
             }
             annotations.append(annotation)
+
+    if args.coco_in is not None:
+        images = coco_gt["images"]
 
     coco_gt_dct = {"images": images, "annotations": annotations, "categories": categories}
     indent = None
