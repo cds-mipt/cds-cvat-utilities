111acf786dbfcaaa8147a2e0c22504d708c00138
Yaroslav Solomentsev <pochta_yarik@mail.ru>
2019-11-18 12:13:51 +0300

    Just copied file from branch "ivb/CDSCV-23"

---
+++ utils.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+import pycocotools.mask as mask_utils
+
+
+def coco_cat_to_label(cat, supercategory=False):
+    label = cat["name"]
+    if supercategory:
+        label = cat["supercategory"] + "/" + cat["name"]
+    label = label.replace(" ", "_")
+    return label
+
+
+def mask_to_polygons(m, approx=0.0):
+    polygons, _ = cv2.findContours(m * 255, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
+    polygons = [cv2.approxPolyDP(polygon, approx, True) for polygon in polygons]
+    polygons = [[point[0].tolist() for point in polygon] for polygon in polygons]
+    return polygons
+
+
+def clip_point(x, y, W, H):
+    x = np.clip(x, 0, W)
+    y = np.clip(y, 0, H)
+    return x, y
+
+
+def invert(rleObjs):
+    masks = mask_utils.decode(rleObjs)
+    masks = 1 - masks
+    return mask_utils.encode(masks)
+
+
+def intersect(rleObjs):
+    rleObjs = invert(rleObjs)
+    i = mask_utils.merge(rleObjs)
+    return invert([i])[0]
+
+
+def difference(a, b):
+    ib = invert([b])[0]
+    return intersect([a, ib])
