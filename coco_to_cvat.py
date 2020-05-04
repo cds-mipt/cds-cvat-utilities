0ee22b7580e41662f188e03833fc97915d9a578c
Yaroslav Solomentsev <pochta_yarik@mail.ru>
2020-05-04 15:32:45 +0300

    corrected to cvat

--- coco_to_cvat.py
+++ coco_to_cvat.py
@@ -10,7 +10,7 @@ import json
 import os
 
 labels = {} #global
-with open("99_Taganrog-Day-Night_cropped_traffic_lights.json", "r") as file:
+with open("mappilary_part_0.json", "r") as file:
     data = json.load(file)
 
 def create_header(im_num):
@@ -59,25 +59,27 @@ def points_layout(img, hash):
     for annot in data["annotations"]:
         cnt = 0
         if (annot["image_id"] == hash and annot["iscrowd"] == 0):
-
-            box_array = annot["bbox"]
-            points = ""
-            seg_len = len(annot["segmentation"][0])
-            while(cnt < seg_len):
-                points += str(annot["segmentation"][0][cnt])
-                points += ","
-                points += str(annot["segmentation"][0][cnt + 1])
-                points += ";"
-                cnt += 2
+            box_array = annot.get("bbox")          
             label_id = annot["category_id"]
-            conf = str(annot['conf'])
+            conf = str(annot.get('conf'))
+            if conf == None:
+                conf = "None"            
+            if "segmentation" in annot.keys():          
+                points = ""
+                seg_len = len(annot["segmentation"][0])
+                while(cnt < seg_len):
+                    points += str(annot["segmentation"][0][cnt])
+                    points += ","
+                    points += str(annot["segmentation"][0][cnt + 1])
+                    points += ";"
+                    cnt += 2
 
-            polygon_sub_element = ET.SubElement(img, "polygon", {"label": labels[label_id], "occluded":"0",\
-                                  "points":points[:-1], 'conf':conf})
-            attribute_polygon_sub_element = ET.SubElement(polygon_sub_element, 'attribute', {})
-            attribute_polygon_sub_element.text = str(conf)
-            attribute_polygon_sub_element.set('name','conf')
-            if (len(box_array) != 0):
+                polygon_sub_element = ET.SubElement(img, "polygon", {"label": labels[label_id], "occluded":"0",\
+                                                    "points":points[:-1], 'conf':conf})
+                attribute_polygon_sub_element = ET.SubElement(polygon_sub_element, 'attribute', {})
+                attribute_polygon_sub_element.text = str(conf)
+                attribute_polygon_sub_element.set('name','conf')
+            if box_array != None:
                 xtl = box_array[0]
                 ytl = box_array[1]
                 lenght = box_array[2]
@@ -88,9 +90,9 @@ def points_layout(img, hash):
                 box_sub_element = ET.SubElement(img, "box", {"label": labels[label_id], "occluded":"0",\
                                "xtl":str(xtl), "ytl":str(ytl),\
                                "xbr":str(xbr), "ybr":str(ybr)})
-                attribute_box_sub_element = ET.SubElement(box_sub_element, 'attribute', {})
-                attribute_box_sub_element.text = str(conf)
-                attribute_box_sub_element.set('name','conf')
+                #attribute_box_sub_element = ET.SubElement(box_sub_element, 'attribute', {})
+                #attribute_box_sub_element.text = str(conf)
+                #attribute_box_sub_element.set('name','conf')
 
 
 
@@ -103,25 +105,19 @@ def f_name(image):
     return image["file_name"]
 
 def main():
-    xml_f = open("99_Taganrog-Day-Night_cropped_traffic_lights.xml", "wb")
+    xml_f = open("mappilary_part_0.xml", "wb")
 
     im_number = len(data["images"])
     create_labels()
     root = create_header(im_number)
 
-    file_list = list()
-    directory = "/home/solomentsev_yaroslav/Jupyter_Notebook/datasets/Cropped_traffic_lights_TAGANROG/images"
-    for filename in os.listdir(directory):
-        file_list.append(filename)
-
     data["images"].sort(key=f_name)
 
     tmp = 0
     for image in data["images"]:
-        if (image["file_name"] in file_list):
-            add_image(image, root, image["width"], image["height"], image["file_name"], image["id"], tmp)
-            create_labels()
-            tmp += 1
+        add_image(image, root, image["width"], image["height"], image["file_name"], image["id"], tmp)
+        create_labels()
+        tmp += 1
 
     tree = ET.ElementTree(root)
     tree.write(xml_f)
